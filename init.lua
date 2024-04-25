6ded45bb5a249ec34f042983aa0d2dbdd68e8d9e
Zachary Hueras <zachary.hueras@gmail.com>
2024-04-25 16:36:24 -0400

    switch to kangawa colorscheme

--- init.lua
+++ init.lua
@@ -790,51 +790,20 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'marko-cerovac/material.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    opts = {
-      contrast = {
-        terminal = true,
-        sidebars = true,
-        floating_windows = true,
-        cursor_line = true,
-        non_current_windows = false,
-      },
-      plugins = {
-        'dashboard',
-        'gitsigns',
-        'neogit',
-        'neo-tree',
-        'nvim-web-devicons',
-        'telescope',
-        'which-key',
-      },
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = { bold = true },
-        variables = { bold = true },
-      },
-      high_visibility = {
-        darker = true,
-      },
-      custom_highlights = {
-        NeoTreeCursorLine = { bg = '#292D3E', fg = '#AB47BC' },
-      },
-    },
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    lazy = false,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'material'
-
-      -- You can configure highlights by doing something like:
-      -- vim.cmd.hi 'Comment gui=none'
+      require('kanagawa').setup {
+        theme = 'dragon',
+        terminalColors = true,
+        background = {
+          dark = 'dragon',
+          light = 'lotus',
+        },
+      }
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
