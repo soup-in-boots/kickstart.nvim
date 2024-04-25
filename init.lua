df2090b612e477c3142ad9a7b46882dde3793fd3
Zachary Hueras <zachary.hueras@gmail.com>
2024-04-25 16:35:03 -0400

    use prettier for formatting

--- init.lua
+++ init.lua
@@ -668,6 +668,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { { 'prettierd', 'prettier' } },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
